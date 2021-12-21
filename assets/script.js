547ea528cd265286bc3d1d54f1299354016a65bc
cornishcoder1 <leahfisher07@hotmail.co.uk>
2021-12-21 10:40:15 +0000

    Add list of arrays, functions etc required into js file

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,30 @@
 console.log('hello')
+
+var story = document.getElementById("story");
+var siteFooter = document.getElementById("siteFooter");
+var question = document.getElementById("question");
+var answer = document.getElementById("answer");
+var yourAnswer = document.getElementById ("yourAnswer");
+var submit = docuument.getElementById ("submit");
+
+// Make an empty array called answers
+
+
+//Listen for clicks on the submit button and call the getAnswer() function when they happen
+
+
+//Call the function to ask the first question 
+
+
+//askQuestion() asks a question, based on the number passed to it
+function askQuestion (questionNumber) {}
+
+
+//getAnswer() gets the answer from the text field and pushes it into the answers array, then calls the continueStory() function
+function getAnswer(){}
+
+//continueStory() displays part of the story or an error based on the value of an item in the answers array
+function continueStory(answerNumber) {}
+
+//theEnd() ends the story and hides the input field
+function theEnd() {}
